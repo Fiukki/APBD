b8a3060540c9cffdadf77f4c7d2350c68f0afe94
Fiukki <s24856@pjwstk.edu.pl>
2025-03-09 20:38:23 +0100

    Średnia

---
+++ Zadanie_3/Zadanie_3/Avg.cs
@@ -0,0 +1,14 @@
+﻿namespace Zadanie_3;
+
+public class Avg
+{
+    public static double Avrage(double[] numbers)
+    {
+        if (numbers.Length == 0 || numbers == null)
+        {
+            throw new ArgumentException("Array cannot be empty or null");
+        }
+
+        return numbers.Average();
+    }
+}
---
+++ Zadanie_3/Zadanie_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Zadanie_3;
+
+class Program
+{
+    static void Main()
+    {
+        double[] numbers = { 1, 2, 3, 4, 5 };
+        double average = Avg.Avrage(numbers);
+        Console.WriteLine(average);
+    }
+}
