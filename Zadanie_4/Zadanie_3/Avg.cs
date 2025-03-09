48b397f2ee9ce775c1b09d51baaf41b03b3154f3
Fiukki <s24856@pjwstk.edu.pl>
2025-03-09 21:02:30 +0100

    Maksymalna

---
+++ Zadanie_4/Zadanie_3/Avg.cs
@@ -0,0 +1,14 @@
+﻿namespace Zadanie_3;
+
+public class Max
+{
+    public static double MaxNumber(double[] numbers)
+    {
+        if (numbers.Length == 0 || numbers == null)
+        {
+            throw new ArgumentException("Array cannot be empty or null");
+        }
+
+        return numbers.Max();
+    }
+}
